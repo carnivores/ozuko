cdc8ef1e17c29c4e48bb174c71de987038dd3114
Dawid Zagorski <zagorskid@o2.pl>
2014-12-03 22:50:39 +0100

    Parallel version

--- openMPZad2_MonteCarlo/openMPZad2_MonteCarlo/openMPZad2_MonteCarlo_parallel.cpp
+++ openMPZad2_MonteCarlo/openMPZad2_MonteCarlo/openMPZad2_MonteCarlo_parallel.cpp
@@ -1,4 +1,4 @@
-// openMPZad2_MonteCarlo_basic.cpp : Defines the entry point for the console application.
+// openMPZad2_MonteCarlo_parallel.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
@@ -10,28 +10,29 @@
 
 using namespace std;
 
-/*
-
 // funkcja dla ktorej obliczana jest calka
-double f(double x) {	
+double f(double x) {
 	if (x != 1)
 	{
-		return (3*(x*x*x) + 2*(x*x) + x) / ((x*x) - 1);
+		return (3 * (x*x*x) + 2 * (x*x) + x) / ((x*x) - 1);
 	}
 	else
 	{
 		return 0;
-	}	
+	}
 }
 
 
 
 int _tmain(int argc, _TCHAR* argv[])
-{	
-	unsigned int n = 25; // liczba punktow
+{
+	unsigned int n = 100000000; // liczba punktow
 	double a = 0; // poczatek przedzialu
 	double b = 0.5; // koniec przedzialu
+	int numberOfThreads = 1;
+	int temp;
 
+	/*
 	cout << "Podaj liczbe punktow (x1000): ";
 	cin >> n;
 	n *= 1000;
@@ -41,27 +42,40 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	cout << "Podaj koniec przedzialu: ";
 	cin >> b;
+	*/
+
+	cout << "Podaj liczbe watkow: ";
+	cin >> temp;
+
+	if (temp != NULL && temp > 0 && temp <= 8)
+	{
+		numberOfThreads = temp;
+	}
+	omp_set_num_threads(numberOfThreads);
 
 	cout << "OK, obliczam calke dla funkcji f(x) = (3*(x*x*x) + 2*(x*x) + x) / ((x*x) - 1)" << endl;
 	cout << "z parametrami: n=" << n << ", a=" << a << " oraz b=" << b << endl;
+	cout << "Liczba watkow: " << numberOfThreads << endl;
 	cout << "Prosze o chwilunie cierpliwosci." << endl;
 	cout << "LICZEM...\n" << endl;
 
 
 	double dx = b - a;
-	
-	double integral;
-	double sum = 0;	
 
-	int threadNumber = 1; // numer watku
-	
+	double integral;
+	double sum = 0;
+	 
+
 	clock_t start = clock();
-	srand((int)time(NULL) * threadNumber);	
-	
+
+	int threadNumber = omp_get_thread_num(); // numer watku
+	srand((int)time(NULL) * threadNumber);
+
+#pragma omp parallel for shared(a, dx, n) reduction(+:sum)
 	for (int i = 1; i < n; ++i)
-	{	
-		sum += f( a + dx*((double)rand()/(double)(RAND_MAX+1)) );		
-	}			
+	{
+		sum += f(a + dx*((double)rand() / (double)(RAND_MAX + 1)));
+	}
 
 	integral = dx / n * sum;
 
@@ -73,4 +87,4 @@ int _tmain(int argc, _TCHAR* argv[])
 	return 0;
 
 }
-*/
+
